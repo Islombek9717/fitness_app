8ea03fbb06a12c5ca0f42db03a9b5ab631c788a1
Islombek <sultonovislom9717@gmail.com>
2022-12-08 03:36:41 +0900

    Starter file structer finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {Route, Routes} from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import './App.css';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Navbar from './components/Navbar';
+
 
 const App = () => {
   return (
-    <div>
-      App
-    </div>
+    <Box width='480px'>
+      <Navbar  />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/exercise/:id' element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
   )
 }
 
