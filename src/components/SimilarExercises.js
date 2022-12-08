d4804043d87595d7f50b36054f3f8b2c13606dd4
Islombek <sultonovislom9717@gmail.com>
2022-12-09 02:35:42 +0900

    finish exerciseVideos component and ceonnect with youtube api

---
+++ src/components/SimilarExercises.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SimilarExercises = () => {
+  return (
+    <div>
+      SE
+    </div>
+  )
+}
+
+export default SimilarExercises
--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -7,6 +7,14 @@ export const exerciseOptions = {
   };
 
   
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '26160fa9c2msh714175ba676e9c6p18c490jsn10abdba15b39',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
+  
 
 export const fetchData = async (url,options) =>  {
     const res = await fetch(url,options);
